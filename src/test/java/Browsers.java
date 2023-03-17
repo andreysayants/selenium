36c14ce2ffb8ce521257e0f248d4608ff10ee7b4
Саянц Андрей Владимирович (4082282) <asayanc@vtb.ru>
2023-03-17 10:21:55 +0300

    Проблема с запуском Opera

--- src/test/java/Browsers.java
+++ src/test/java/Browsers.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera
+import org.openqa.selenium.Opera;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
