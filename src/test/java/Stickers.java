c796da66ee1dd58f63f987e2ea13bce86e25e2a6
Саянц Андрей Владимирович (4082282) <asayanc@vtb.ru>
2023-03-28 08:52:56 +0300

    Добавил задачу на Stickers

---
+++ src/test/java/Stickers.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Stickers {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void start() {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+    }
+
+    @Test
+    public void stickersTest() {
+        driver.get("http://localhost/litecart/en/");
+        wait.until(ExpectedConditions.attributeToBe(By.id("box-most-popular"), "", ""));
+
+        int duckCount = driver.findElements(By.cssSelector(".product")).size();
+        for (int i = 0; i < duckCount; i++) {
+            List<WebElement> items = driver.findElements(By.cssSelector(".product"));
+            WebElement item = items.get(i);
+            int sticker = item.findElements(By.cssSelector(".sticker")).size();
+            assertEquals(1, sticker);
+        }
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
