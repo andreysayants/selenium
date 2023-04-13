c7c80bb9ca40543a420146f0e244a3f3b502faac
Саянц Андрей Владимирович (4082282) <asayanc@vtb.ru>
2023-04-13 09:48:41 +0300

    Задание 17. Проверьте отсутствие сообщений в логе браузера MyStoreAdminLogs

---
+++ src/test/java/MyStoreAdminLogs.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyStoreAdminLogs {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void start() {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void logsTest() {
+//        Login
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+//        Catalog
+        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+        List<WebElement> count = driver.findElements(By.xpath("//*[@style='text-align: right;']/following-sibling::td[a]"));
+        for (int i = 0; i < count.size(); i++) {
+            List<WebElement> rows = driver.findElements(By.xpath("//*[@style='text-align: right;']/following-sibling::td[a]"));
+            rows.get(i).click();
+            List<LogEntry> logs = driver.manage().logs().get("browser").getAll();
+            assertTrue(logs.isEmpty());
+            driver.navigate().back();
+
+        }
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
